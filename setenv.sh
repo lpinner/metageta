07b77d1ed0871332cc269a07cee4f08fbc7c8119
pinner.luke <pinner.luke@c40560ac-0007-11df-bce7-934f6fdf4811>
2010-03-16 06:26:24 +0000

    Reverting r230.

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+export VERSION=2.0
 export CURDIR=$(cd "$(dirname "$0")"; pwd)
-#export GDAL_DATA=$CURDIR/../gdal_data
-export GDAL_DATA=/usr/share/gdal16
+export GDAL_DATA=$CURDIR/../gdal_data
 export GEOTIFF_CSV=$GDAL_DATA
 export PYTHONPATH=$CURDIR/lib:$PYTHONPATH
 

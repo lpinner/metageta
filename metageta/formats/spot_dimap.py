7153ea3d9f5846ba2b0a710ada97b054ce6c8725
pinner.luke <pinner.luke@c40560ac-0007-11df-bce7-934f6fdf4811>
2012-05-24 01:51:09 +0000

    port SPOT DIMAP driver to lxml instead of 4suite

--- metageta/formats/spot_dimap.py
+++ metageta/formats/spot_dimap.py
@@ -30,7 +30,7 @@ B{Format specification}:
 
 format_regex=[r'metadata\.dim$'] #SPOT DIMAP
 '''Regular expression list of file formats'''
-    
+
 #import base dataset modules
 #import __dataset__
 import __default__
@@ -38,8 +38,7 @@ import __default__
 # import other modules
 import sys, os, re, glob, time, math, string
 from metageta import utilities, geometry
-#import xml.dom.minidom as _xmldom
-from Ft.Xml import Parse
+from lxml import etree
 
 try:
     from osgeo import gdal
@@ -53,7 +52,7 @@ except ImportError:
     import ogr
 gdal.AllRegister()
 
-class Dataset(__default__.Dataset): 
+class Dataset(__default__.Dataset):
     '''Subclass of __default__.Dataset class so we get a load of metadata populated automatically'''
     def __init__(self,f):
         '''Open the dataset'''
@@ -62,13 +61,13 @@ class Dataset(__default__.Dataset):
     def __getmetadata__(self,f=None):
         '''Read Metadata for a SPOT DIMAP image as GDAL doesn't quite get it all...'''
         if not f:f=self.fileinfo['filepath']
-        #dom=__xmldom.parse(f) #Takes tooo long to parse the whole file, so just read as far as we need...
+        #dom=etree.parse(f) #Takes tooo long to parse the whole file, so just read as far as we need...
         strxml=''
         for line in open(f, 'r'):
             if line.upper().strip()=='<DATA_STRIP>':break
             else: strxml+=line
         if not '</Dimap_Document>' in strxml:strxml+='</Dimap_Document>'
-        dom=Parse(strxml)
+        dom=etree.fromstring(strxml)
         #self.metadata['sceneid'] = dom.documentElement.getElementsByTagName('DATASET_NAME')[0].childNodes[0].data
         #bands=dom.documentElement.getElementsByTagName('BAND_DESCRIPTION')
         #self.metadata['bands']=','.join([band.childNodes[0].data for band in bands])
@@ -80,19 +79,17 @@ class Dataset(__default__.Dataset):
         except geometry.GDALError,err: #Work around reading images with lowercase filenames when
                                        #the DATA_FILE_PATH is uppercase
                                        # - eg samba filesystems which get converted to lowercase
-            #dfp=dom.documentElement.getElementsByTagName('DATA_FILE_PATH')[0]
-            #fn=str(dfp.getAttribute('href'))
+
             dfp=dom.xpath('/Dimap_Document/Data_Access/Data_File/DATA_FILE_PATH')[0]
             fn=utilities.encode(dfp.xpath('string(@href)')) #XML is unicode, gdal.Open doesn't like unicode
             if not os.path.dirname(fn):fn=os.path.join(os.path.dirname(f),fn)
             exists,img=utilities.exists(fn,True)
             if exists and not os.path.exists(fn):
                 import tempfile
-                from Ft.Xml.Domlette import PrettyPrint
                 tmpfd,tmpfn=tempfile.mkstemp(suffix='.dim',prefix='metadata')
-                dfp.xpath('@href')[0].value=img
+                dfp.set('href',img)
                 tmpfo=os.fdopen(tmpfd,'w')
-                PrettyPrint(dom,tmpfo)
+                tmpfo.write(etree.tostring(dom))
                 tmpfo.flush()
                 tmpfo.close()
                 cwd=os.path.abspath(os.curdir)
@@ -108,7 +105,7 @@ class Dataset(__default__.Dataset):
         dates={}
         for src in dom.xpath('//Scene_Source'):
             datetime='%sT%s'%(src.xpath('string(IMAGING_DATE)'),src.xpath('string(IMAGING_TIME)'))
-            dts=time.mktime(time.strptime(datetime,utilities.datetimeformat))#ISO 8601 
+            dts=time.mktime(time.strptime(datetime,utilities.datetimeformat))#ISO 8601
             dates[dts]=datetime
 
         self.metadata['imgdate']='%s/%s'%(dates[min(dates.keys())],dates[max(dates.keys())])
@@ -124,7 +121,7 @@ class Dataset(__default__.Dataset):
         except:pass
         self.metadata['viewangle'] = gdalmd.get('VIEWING_ANGLE',gdalmd.get('INCIDENCE_ANGLE',''))
 
-    def getoverview(self,outfile=None,width=800,format='JPG'): 
+    def getoverview(self,outfile=None,width=800,format='JPG'):
         '''
         Generate overviews for SPOT imagery
 

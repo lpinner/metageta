9b2cf6935be34581fa6839c2fee960475b562969
lukepinnerau <lukepinnerau@gmail.com>
2014-04-10 04:30:05 +0000

    Fix borked tkinter in pyscripter.cmd Remove duplicate line in setenv.bat

--- pyscripter.cmd
+++ pyscripter.cmd
@@ -2,8 +2,10 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 call "%~DP0\setenv.bat"
 set PYTHONDLL=%PYTHONHOME%
+set TCL_LIBRARY=%PYTHONHOME%\tcl\tcl8.5
+set TK_LIBRARY=%PYTHONHOME%\tcl\tk8.5
+set PYTHONPATH=%PYTHONPATH%;%PYTHONHOME%\DLLs;C:\Software\PyScripter\Lib
 set PYTHONHOME=
-set PYTHONPATH=%PYTHONPATH%;C:\Software\PyScripter\Lib
 REM start "PyScripter MetaGETA" /B "C:\Software\PyScripter\PyScripter.exe" --PYTHON27  --PYTHONDLLPATH "%PYTHONDLL%" --newinstance %1 %2 %3 %4 %5
 CALL "C:\Software\PyScripter\PyScripter.exe" --PYTHON27  --PYTHONDLLPATH "%PYTHONDLL%" --newinstance %1 %2 %3 %4 %5
 PAUSE

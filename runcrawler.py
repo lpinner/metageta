f1f4129e7dd08e7bbc954eb7d7054c246a7a4fdb
lukepinnerau <lukepinnerau@gmail.com>
2014-08-18 04:59:01 +0000

    Strip out progress logger gui code.

--- runcrawler.py
+++ runcrawler.py
@@ -412,8 +412,7 @@ if __name__ == '__main__':
     else: #No need for the GUI
         xls = utilities.checkExt(utilities.encode(optvals.xls), ['.xls'])
         log=xls.replace('.xls','.log')
-        #logger=getlogger(log, name=APP, debug=optvals.debug, icon=ICON)
-        logger=getlogger(log, name=APP, debug=optvals.debug, icon=ICON)
+        logger=getlogger(log, name=APP, debug=optvals.debug)
         main(optvals.dir,xls,logger,optvals.med,optvals.update,optvals.ovs,optvals.recurse,optvals.archive)
 
     if logger:
